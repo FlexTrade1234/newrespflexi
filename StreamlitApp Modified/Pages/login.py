5dc3678518fae0c2be58fd402acef12ce88dd5a9
sreelatha <krishna33.a.s@gmail.com>
2025-01-22 15:40:58 +0100

    Create login.py

---
+++ StreamlitApp Modified/Pages/login.py
@@ -0,0 +1,24 @@
+import streamlit as st
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+from db import checkdata
+from Pages.globals import insert,get
+
+
+st.title("Login")
+usr=st.text_input("Username")
+password=st.text_input("Password",type="password")
+
+
+if(st.button("Login")):
+    Obj1=[usr,password]
+    if(checkdata("Users",Obj1)):
+        insert("user",usr)
+        print("Hi i am global user now- ",get("user"))
+        st.success('Login Successfully!', icon="✅")
+        st.balloons()
+        st.rerun()
+    else:
+        st.error('Error in Login', icon="🚨")
+
